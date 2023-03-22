9e802f23d587b586201fbf8560fc18e2d3c8ada0
aaranyak <aaranyak.ghosh@gmail.com>
2023-03-22 15:36:40 +0530

    Removed unused imports in fin_plate_connection.py

--- osdag_api/modules/fin_plate_connection.py
+++ osdag_api/modules/fin_plate_connection.py
@@ -1,7 +1,4 @@
-import os
-from design_type.connection.fin_plate_connection import FinPlateConnection
-from gui.ui_template import Window, Ui_ModuleWindow
-from Common import *
+from design_type.connection.fin_plate_connection import FinPlateConnectionfrom gui.ui_template import Window, Ui_ModuleWindow
 import typing
 import os
 os.system("clear")
