17528616e4932b8d0a22519859abcbb4efce6a88
aaranyak <aaranyak.ghosh@gmail.com>
2023-03-22 15:58:59 +0530

    Corrected import in fin_plate_connection.py

--- osdag_api/modules/fin_plate_connection.py
+++ osdag_api/modules/fin_plate_connection.py
@@ -1,4 +1,4 @@
-from design_type.connection.fin_plate_connection import FinPlateConnectionfrom gui.ui_template import Window, Ui_ModuleWindow
+from design_type.connection.fin_plate_connection import FinPlateConnection
 import typing
 import os
 os.system("clear")
