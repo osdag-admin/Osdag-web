83783303c47b7e933294b78eed5ec562821981e4
aaranyak <aaranyak.ghosh@gmail.com>
2023-03-23 10:08:58 +0530

    Added state and input value fields to DesignSession Model

--- osdag/models.py
+++ osdag/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
 class Design(models.Model):
     """Design Session object in Database."""
-    cookie_id = models.CharField(unique=True, max_length=200)
+    cookie_id = models.CharField(unique=True, max_length=32)
     module_id = models.CharField(max_length=200)
+    current_state = models.CharField(default="IV", max_length=2)
+    input_values = models.TextField(blank=True)
