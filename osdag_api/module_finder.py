bd88e865a81cc4aaf0ea031c419937361255a1b4
aaranyak <aaranyak.ghosh@gmail.com>
2023-03-26 15:42:16 +0530

    Added module api protocol

--- osdag_api/module_finder.py
+++ osdag_api/module_finder.py
@@ -1,10 +1,36 @@
+from design_type.main import Main
 from osdag_api.modules import fin_plate_connection
 from types import ModuleType
 import typing
-module_dict = {
+from typing import Dict, Any, List, Protocol
+class ModuleApiType(Protocol):
+    def validate_input(self, input_values: Dict[str, Any]) -> None:
+        """Validate type for all values in design dict. Raise error when invalid"""
+        pass
+    def get_required_keys(self) -> List[str]:
+        pass
+    def create_module(self) -> Main:
+        """Create an instance of themodule design class and set it up for use"""
+        pass
+    def create_from_input(self, input_values: Dict[str, Any]) -> Main:
+        """Create an instance of the module design class from input values."""
+        pass
+    def generate_ouptut(self, input_values: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Generate, format and return the input values from the given output values.
+        Output format (json): {
+            "Bolt.Pitch": 
+                "key": "Bolt.Pitch",
+                "label": "Pitch Distance (mm)"
+                "value": 40
+            }
+        }
+        """
+        pass
+module_dict : Dict[str, ModuleApiType] = {
     'Fin Plate Connection': fin_plate_connection
-}
-def get_module_api(module_id: str) -> ModuleType:
+} 
+def get_module_api(module_id: str) -> ModuleApiType:
     """Return the api for the specified module"""
     module = module_dict[module_id]
     return module
