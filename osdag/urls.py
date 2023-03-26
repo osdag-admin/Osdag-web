4151af95f78ebab8e2b6ca87180dfd873abcf003
aaranyak <aaranyak.ghosh@gmail.com>
2023-03-26 22:01:09 +0530

    Created Output Data API:     file: osdag/web_api/output_data_api.py     Output Values API (class OutputValues(View)):         Accepts GET requests.         Returns content_type application/json         Request must provide session cookie id.         Data Format: (json): {             "Bolt.Pitch":                 "key": "Bolt.Pitch",                 "label": "Pitch Distance (mm)"                 "value": 40             }         }

--- osdag/urls.py
+++ osdag/urls.py
@@ -3,6 +3,8 @@ from django.urls import include
 from osdag.web_api.session_api import CreateSession
 from osdag.web_api.session_api import DeleteSession
 from osdag.web_api.input_data_api import InputValues
+from osdag.web_api.output_data_api import OutputValues
+
 urlpatterns = [
     path('sessions/create/', CreateSession.as_view()),
     path('sessions/create', CreateSession.as_view()),
@@ -10,4 +12,6 @@ urlpatterns = [
     path('sessions/delete', DeleteSession.as_view()),
     path('design/input_values/', InputValues.as_view()),
     path('design/input_values', InputValues.as_view()),
+    path('design/onput_values/', OutputValues.as_view()),
+    path('design/output_values', OutputValues.as_view()),
 ]
