d659986b123e0bdb13b77aa82e105431049c3def
aaranyak <aaranyak.ghosh@gmail.com>
2023-03-30 12:53:16 +0530

    Corrected view class in urls.py

--- osdag/urls.py
+++ osdag/urls.py
@@ -4,7 +4,7 @@ from osdag.web_api.session_api import CreateSession
 from osdag.web_api.session_api import DeleteSession
 from osdag.web_api.input_data_api import InputValues
 from osdag.web_api.output_data_api import OutputValues
-
+from osdag.web_api.cad_model_api import CADGeneration
 urlpatterns = [
     path('sessions/create/', CreateSession.as_view()),
     path('sessions/create', CreateSession.as_view()),
@@ -14,6 +14,6 @@ urlpatterns = [
     path('design/input_values', InputValues.as_view()),
     path('design/output_values/', OutputValues.as_view()),
     path('design/output_values', OutputValues.as_view()),
-    path('design/cad/', OutputValues.as_view()),
-    path('design/cad', OutputValues.as_view()),
+    path('design/cad/', CADGeneration.as_view()),
+    path('design/cad', CADGeneration.as_view()),
 ]
