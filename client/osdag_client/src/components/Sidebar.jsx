5678a5efc1e8e9699c318df62072ceb1bc9d5618
sai charan <saicharan0662@gmail.com>
2023-06-03 15:28:18 +0530

    root api intigration

--- client/osdag_client/src/components/Sidebar.jsx
+++ client/osdag_client/src/components/Sidebar.jsx
@@ -6,19 +6,19 @@ import SidebarJSON from '../urls/Sidebar.json'
 function Sidebar() {
 
 
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
     fetchData();
   }, []);
   const fetchData = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:8000/osdag-web/',{
+      const response = await fetch('http://127.0.0.1:8000/osdag-web/', {
         method: 'GET'
-        });
+      });
       const jsonData = await response.json();
       console.log(JSON.stringify(jsonData))
-      setData(jsonData);
+      setData(jsonData.result);
     } catch (error) {
       console.log('Error fetching data:', error);
     }
@@ -30,99 +30,47 @@ function Sidebar() {
     if (selectedOptionValue === "1") {
       window.location.href = "https://osdag.fossee.in/resources/videos";
     }
-    else if(selectedOptionValue === "2"){
+    else if (selectedOptionValue === "2") {
       window.location.href = "https://www.youtube.com/channel/UCnSZ7EjhDwNi3eCPcSKpgJg";
     }
-    else if(selectedOptionValue === "3"){
+    else if (selectedOptionValue === "3") {
       window.location.href = "file:///D:/Osdag_windows_installer_v2021.02.a.a12f/Osdag_windows_installer_v2021.02.a.a12f/Osdag/ResourceFiles/html_page/_build/html/index.html";
     }
-    else if(selectedOptionValue === "4"){
+    else if (selectedOptionValue === "4") {
       window.location.href = "https://osdag.fossee.in/forum";
     }
   }
-    
-    const navigate = useNavigate();
+
+  const navigate = useNavigate();
   return (
-    <> 
-    <div className="sidebar-container">   
+    <>
+      <div className="sidebar-container">
         <div className="sidebar-item-logo">
-           <center> <img src={Osdag_logo} alt="Logo" height="50px" onClick={()=>navigate("/")}/></center>
+          <center> <img src={Osdag_logo} alt="Logo" height="50px" onClick={() => navigate("/")} /></center>
         </div>
-
-{/* 
-{
-  fetch('http://127.0.0.1:8000/osdag-web')
-  .then(response => response.json())
-  .then(data => {
-    if (data.success) {
-      data.result.data.map(item => (
-        <div key={item.id} className="sidebar-item">
-          <button onClick={() => navigate(item.name.replace(/_/g, ""))}>
-            {item.name}
-          </button>
-        </div>
-      ))
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  })
-} */}
-
-
-        {SidebarJSON.Main_Sidebar.map((item, index) => (
-        <div key={index} className="sidebar-item">
-          <button onClick={() => navigate(item.onClick)}>
-            {item.text}
-          </button>
-        </div>
-      ))}
-
-        {/* <div className="sidebar-item">
-          <button onClick={()=>navigate("/connection")}>Connection</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/tenstionMember")}>Tenstion Member</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/notAvailable")}>Compression Member</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/notAvailable")}>Flexural Member</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/notAvailable")}>Beam-Column</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/notAvailable")}>Plate Girder</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/notAvailable")}>Truss</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/notAvailable")}>2D Frame</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/notAvailable")}>3D Frame</button>
-        </div>
-        <div className="sidebar-item">
-          <button onClick={()=>navigate("/notAvailable")}>Group Design</button>
-        </div> */}
-
+        {
+          data && data.data.map((item, index) => {
+            return (
+              <>
+                <div key={index} className="sidebar-item" >
+                  <button onClick={() => {
+                    navigate(item.name.toLowerCase().replaceAll("_", ""))
+                  }}>{item.name}</button>
+                </div>
+              </>
+            )
+          })
+        }
       </div>
-
-            <div className="sidebar-setting">
-              <select onChange={handleSelectChange}>
-                <option value={"1"}>Help</option>
-                <option value={"2"}>Video Tutorials</option>
-                <option value={"3"}>Design Examples</option>
-                <option value={"4"}>Ask Us a Question</option>
-                <option value={"5"}>Check for Update</option>          
-              </select>
-              {/* <h5>Help</h5> */}
-            
-            </div>
-
+      <div className="sidebar-setting">
+        <select onChange={handleSelectChange}>
+          <option value={"1"}>Help</option>
+          <option value={"2"}>Video Tutorials</option>
+          <option value={"3"}>Design Examples</option>
+          <option value={"4"}>Ask Us a Question</option>
+          <option value={"5"}>Check for Update</option>
+        </select>
+      </div>
     </>
   )
 }
