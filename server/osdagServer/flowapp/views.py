1947226f8d5228a795d3adc26d66ba4bdd013928
sai charan <saicharan0662@gmail.com>
2023-06-03 13:53:48 +0530

    get_design_types api created

--- server/osdagServer/flowapp/views.py
+++ server/osdagServer/flowapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http.response import JsonResponse
+from rest_framework.decorators import api_view
+import json
+
+# importing data
+from .data.design_types import design_type_data
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the flowapp index.")
+@api_view(['GET'])
+def get_design_types(request):
+    return JsonResponse({'result': design_type_data}, safe=False)
