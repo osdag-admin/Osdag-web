57f9408b1726519d323c90de7fa0d0bec7e0e980
sai charan <saicharan0662@gmail.com>
2023-06-03 16:49:38 +0530

    ui changes

--- client/osdag_client/src/components/Connection.jsx
+++ client/osdag_client/src/components/Connection.jsx
@@ -1,5 +1,5 @@
 import '../App.css'
-import {useState} from "react";
+import { useState } from "react";
 // Shear Connection
 import sc1 from "../assets/ShearConnection/1.png";
 import sc2 from "../assets/ShearConnection/2.png";
@@ -52,28 +52,28 @@ function Connection() {
     <div className="container">
       <div className="bloc-tabs">
         <button
-          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          className={toggleState === 1 ? "tab-btn tabs active-tabs" : "tab-btn tabs"}
           onClick={() => toggleTab(1)}
         >
           Shear Connection
         </button>
         <button
-          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          className={toggleState === 2 ? "tab-btn tabs active-tabs" : "tab-btn tabs"}
           onClick={() => toggleTab(2)}
         >
-          Moment Connection 
+          Moment Connection
         </button>
         <button
-          className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          className={toggleState === 3 ? "tab-btn tabs active-tabs" : "tab-btn tabs"}
           onClick={() => toggleTab(3)}
         >
-          Base Plate 
+          Base Plate
         </button>
         <button
-          className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+          className={toggleState === 4 ? "tab-btn tabs active-tabs" : "tab-btn tabs"}
           onClick={() => toggleTab(4)}
         >
-          Truss Connection 
+          Truss Connection
         </button>
       </div>
 
@@ -83,95 +83,95 @@ function Connection() {
         >
           <center><h2> Shear Connection</h2></center>
           <hr />
-            <div className='conn-grid-container'>
-              <div className='conn-grid-item'><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Fin Plate</b><br/><img src={sc1}/></div> 
-              <div className='conn-grid-item'><input type="radio" value="Cheat_Angle" name="shear-conn" /><b>Cleat Angle</b><br/> <img src={sc2}/></div>
-              <div className='conn-grid-item'><input type="radio" value="End_Plate" name="shear-conn" /><b>End Plate</b><br/> <img src={sc3}/></div>
-              <div className='conn-grid-item'><input type="radio" value="Seated_Angle" name="shear-conn" /><b>Seated Angle</b><br/> <img src={sc4}/></div>
-            </div>
+          <div className='conn-grid-container'>
+            <div className='conn-grid-item'><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Fin Plate</b><br /><img src={sc1} /></div>
+            <div className='conn-grid-item'><input type="radio" value="Cheat_Angle" name="shear-conn" /><b>Cleat Angle</b><br /> <img src={sc2} /></div>
+            <div className='conn-grid-item'><input type="radio" value="End_Plate" name="shear-conn" /><b>End Plate</b><br /> <img src={sc3} /></div>
+            <div className='conn-grid-item'><input type="radio" value="Seated_Angle" name="shear-conn" /><b>Seated Angle</b><br /> <img src={sc4} /></div>
+          </div>
 
-            <center><div className='conn-btn'><button>Start</button></div></center>
+          <center><div className='conn-btn'><button>Start</button></div></center>
         </div>
 
         <div
           className={toggleState === 2 ? "content  active-content" : "content"}
         >
           {/* +++++++++++++++++++ */}
-            <div className="container">
-      <div className="bloc-tabs">
-        <button
-          className={subtoggleState === 1 ? "tabs active-tabs" : "tabs"}
-          onClick={() => subtoggleTab(1)}
-        >
-          Beam-to-Beam Splice
-        </button>
-        <button
-          className={subtoggleState === 2 ? "tabs active-tabs" : "tabs"}
-          onClick={() => subtoggleTab(2)}
-        >
-          Beam-to-Column
-        </button>
-        <button
-          className={subtoggleState === 3 ? "tabs active-tabs" : "tabs"}
-          onClick={() => subtoggleTab(3)}
-        >
-          Column-to-Column Splice
-        </button>
-        <button
-          className={subtoggleState === 4 ? "tabs active-tabs" : "tabs"}
-          onClick={() => subtoggleTab(4)}
-        >
-          PEB
-        </button>
-      </div>
+          <div className="container">
+            <div className="bloc-tabs">
+              <button
+                className={subtoggleState === 1 ? "tab-btn tabs active-tabs" : "tab tabs"}
+                onClick={() => subtoggleTab(1)}
+              >
+                Beam-to-Beam Splice
+              </button>
+              <button
+                className={subtoggleState === 2 ? "tab-btn tabs active-tabs" : "tab-btn tabs"}
+                onClick={() => subtoggleTab(2)}
+              >
+                Beam-to-Column
+              </button>
+              <button
+                className={subtoggleState === 3 ? "tab-btn tabs active-tabs" : "tab-btn tabs"}
+                onClick={() => subtoggleTab(3)}
+              >
+                Column-to-Column Splice
+              </button>
+              <button
+                className={subtoggleState === 4 ? "tab-btn tabs active-tabs" : "tab-btn tabs"}
+                onClick={() => subtoggleTab(4)}
+              >
+                PEB
+              </button>
+            </div>
 
-      <div className="content-tabs">
-        <div
-          className={subtoggleState === 1 ? "content  active-content" : "content"}
-        >
-          <center><h2>Beam-to-Beam Splice</h2></center>
-          <hr />
-          <div className='conn-grid-container'>
-              <div className='conn-grid-item'><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Cover Plate Bolted</b><br/><img src={mcbb1}/></div> 
-              <div className='conn-grid-item'><input type="radio" value="Cheat_Angle" name="shear-conn" /><b>Cover Plate Welded</b><br/> <img src={mcbb2}/></div>
-              <div className='conn-grid-item'><input type="radio" value="Seated_Angle" name="shear-conn" /><b>End Plate</b><br/> <img src={mcbb3}/></div>
-            </div>
-            <center><div className='conn-btn'><button>Start</button></div></center>
-        </div>
+            <div className="content-tabs">
+              <div
+                className={subtoggleState === 1 ? "tab-btn content active-content" : "tab-btn content"}
+              >
+                <center><h2>Beam-to-Beam Splice</h2></center>
+                <hr />
+                <div className='conn-grid-container'>
+                  <div className='conn-grid-item'><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Cover Plate Bolted</b><br /><img src={mcbb1} /></div>
+                  <div className='conn-grid-item'><input type="radio" value="Cheat_Angle" name="shear-conn" /><b>Cover Plate Welded</b><br /> <img src={mcbb2} /></div>
+                  <div className='conn-grid-item'><input type="radio" value="Seated_Angle" name="shear-conn" /><b>End Plate</b><br /> <img src={mcbb3} /></div>
+                </div>
+                <center><div className='conn-btn'><button>Start</button></div></center>
+              </div>
 
-        <div
-          className={subtoggleState === 2 ? "content  active-content" : "content"}
-        >
-          <center><h2>Beam-to-Column</h2></center>
-          <hr />
-          <div className='conn-grid-container'>
-              <div className='conn-grid-item solo-item' ><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Cover Plate Bolted</b><br/><img src={mcbc1}/></div> 
+              <div
+                className={subtoggleState === 2 ? "content  active-content" : "content"}
+              >
+                <center><h2>Beam-to-Column</h2></center>
+                <hr />
+                <div className='conn-grid-container'>
+                  <div className='conn-grid-item solo-item' ><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Cover Plate Bolted</b><br /><img src={mcbc1} /></div>
+                </div>
+                <center><div className='conn-btn'><button>Start</button></div></center>
+              </div>
+              <div
+                className={subtoggleState === 3 ? "content  active-content" : "content"}
+              >
+                <center><h2>Column-to-Column Spice</h2></center>
+                <hr />
+                <div className='conn-grid-container'>
+                  <div className='conn-grid-item'><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Cover Plate Bolted</b><br /><img src={mccc1} /></div>
+                  <div className='conn-grid-item'><input type="radio" value="Cheat_Angle" name="shear-conn" /><b>Cover Plate Welded</b><br /> <img src={mccc2} /></div>
+                  <div className='conn-grid-item'><input type="radio" value="Seated_Angle" name="shear-conn" /><b>End Plate</b><br /> <img src={mccc3} /></div>
+                </div>
+                <center><div className='conn-btn'><button>Start</button></div></center>
+              </div>
+              <div
+                className={subtoggleState === 4 ? "content  active-content" : "content"}
+              >
+                <center><h2>PEB</h2></center>
+                <hr />
+                <p>
+                  This Model is currently under Development !!!
+                </p>
+              </div>
             </div>
-            <center><div className='conn-btn'><button>Start</button></div></center>
-        </div>
-        <div
-          className={subtoggleState === 3 ? "content  active-content" : "content"}
-        >
-          <center><h2>Column-to-Column Spice</h2></center>
-          <hr />
-          <div className='conn-grid-container'>
-              <div className='conn-grid-item'><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Cover Plate Bolted</b><br/><img src={mccc1}/></div> 
-              <div className='conn-grid-item'><input type="radio" value="Cheat_Angle" name="shear-conn" /><b>Cover Plate Welded</b><br/> <img src={mccc2}/></div>
-              <div className='conn-grid-item'><input type="radio" value="Seated_Angle" name="shear-conn" /><b>End Plate</b><br/> <img src={mccc3}/></div>
-            </div>
-            <center><div className='conn-btn'><button>Start</button></div></center>
-        </div>
-        <div
-          className={subtoggleState === 4 ? "content  active-content" : "content"}
-        >
-          <center><h2>PEB</h2></center>
-          <hr />
-          <p>
-            This Model is currently under Development !!!
-          </p>
-        </div>
-      </div>
-    </div>
+          </div>
         </div>
         <div
           className={toggleState === 3 ? "content  active-content" : "content"}
@@ -179,10 +179,10 @@ function Connection() {
           <center><h2>Base Plate</h2></center>
           <hr />
           <div className='conn-grid-container'>
-              <div className='conn-grid-item'><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Base Plate Connection</b><br/><img src={bp1}/></div> 
-          
-            </div>
-            <center><div className='conn-btn'><button>Start</button></div></center>
+            <div className='conn-grid-item'><input type="radio" value="Fin_Plate" name="shear-conn"></input><b>Base Plate Connection</b><br /><img src={bp1} /></div>
+
+          </div>
+          <center><div className='conn-btn'><button>Start</button></div></center>
         </div>
         <div
           className={toggleState === 4 ? "content  active-content" : "content"}
@@ -190,7 +190,7 @@ function Connection() {
           <center><h2>Truss Connection</h2></center>
           <hr />
           <p>
-            
+
             This Model is currently under Development !!!
           </p>
         </div>
