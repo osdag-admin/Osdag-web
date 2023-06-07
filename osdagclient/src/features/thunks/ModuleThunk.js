523e54349f1ec1bb0e6c3349cffda37b0074f094
sai charan <saicharan0662@gmail.com>
2023-06-07 13:00:50 +0530

    built thunks and fixed minor bug in sidebar

--- osdagclient/src/features/thunks/ModuleThunk.js
+++ osdagclient/src/features/thunks/ModuleThunk.js
@@ -7,11 +7,50 @@ const BASE_URL = 'http://127.0.0.1:8000/'
 
 // module thunk
 export const getModules = createAsyncThunk(
-    'moduleSlice/getModules' , async (item) => {
+    'moduleSlice/getModules', async (item) => {
         console.log('inside the thunk')
         const URL = BASE_URL + "osdag-web/"
-        const response = await fetch(URL , {
-            method : "GET"
+        const response = await fetch(URL, {
+            method: "GET"
+        })
+        const jsonData = await response.json();
+        console.log(jsonData.result)
+        return jsonData?.result
+    }
+)
+
+export const getDesignTypes = createAsyncThunk(
+    'moduleSlice/getDesignTypes', async (item) => {
+        console.log('inside the thunk')
+        const URL = BASE_URL + `osdag-web/designTypes/${item}`
+        const response = await fetch(URL, {
+            method: "GET"
+        })
+        const jsonData = await response.json();
+        console.log(jsonData.result)
+        return jsonData?.result
+    }
+)
+
+export const getSubDesignTypes = createAsyncThunk(
+    'moduleSlice/getSubDesignTypes', async (item) => {
+        console.log('inside the thunk')
+        const URL = BASE_URL + `osdag-web/${item.conType}/${item.name.toLowerCase().replaceAll("_", '-')}`
+        const response = await fetch(URL, {
+            method: "GET"
+        })
+        const jsonData = await response.json();
+        console.log(jsonData.result)
+        return jsonData?.result
+    }
+)
+
+export const getLeafLevelDesignType = createAsyncThunk(
+    'moduleSlice/getLeafLevelDesignType', async (item) => {
+        console.log('inside the thunk')
+        const URL = BASE_URL + `osdag-web/${item.conType}/${item.name.toLowerCase().replaceAll("_", '-')}/${item.subDesignType.toLowerCase().replaceAll("_", '-')}`
+        const response = await fetch(URL, {
+            method: "GET"
         })
         const jsonData = await response.json();
         console.log(jsonData.result)
