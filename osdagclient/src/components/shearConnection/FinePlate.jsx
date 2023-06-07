6a0052f269c77df3adfaf4b07d3ea7c9850136d2
Suraj Ranajit Bhosale <surajranajitbhosale003@gmail.com>
2023-06-07 15:28:41 +0530

    fineplate ui and design

--- osdagclient/src/components/shearConnection/FinePlate.jsx
+++ osdagclient/src/components/shearConnection/FinePlate.jsx
@@ -111,6 +111,21 @@ function FinePlate() {
     }
   ];
 
+  const Connectivity = [
+    {
+      "connID": "Column_Flange-Beam_Web",
+      "Data": "Column Flange-Beam Web"
+    },
+    {
+      "connID": "Column_web-Beam_Web",
+      "Data": "Column web-Beam Web"
+    },
+    {
+      "connID": "Beam-Beam",
+      "Data": "Beam-Beam"
+    }
+  ];
+
   console.log(beamData);
     
   console.log(material);
@@ -134,15 +149,18 @@ function FinePlate() {
     {/* Main Body of code  */}
     <div className='superMainBody'>
       {/* Left */}
+      <div>
+        <h5>Input Dock</h5>
       <div className='subMainBody'> 
+      {/* Section 1 Start */}
           <h3>Connecting Members</h3>       
           <div className='component-grid'>
             <div><h4>Connectivity</h4></div>
             
-            <div><Select style={ {width:'200px'}}>
-                <option value={"CFBW"}>Column Flange-Beam Web</option>
-                <option value={"CWBW"}>Column web-Beam Web</option>
-                <option value={"BB"}>Beam-Beam</option>             
+            <div><Select style={ {width:'100%'}}>
+                  {Connectivity.map((item) => (
+                  <option key={item.connID} value={item.connID}>{item.Data}</option>   
+                  ))}   
                 </Select>
             </div>
             
@@ -152,61 +170,151 @@ function FinePlate() {
               <img src={img1} alt="Component" height='100px' width='100px' />
             </div>
 
-            <div><h4>Primary Beam</h4></div>
-            <div><Select style={ {width:'200px'}}>
-                <option value={"CFBW"}>Select Section</option>
-                <option value={"CFBW"}>JB 150</option>
-                <option value={"CFBW"}>JB 175</option>
-                <option value={"CFBW"}>JB 200</option>
-                <option value={"CFBW"}>JB 225</option>
+            <div><h4>Column Section:</h4></div>
+            <div><Select style={ {width:'100%'}}>
+                  {column.map((item) => (
+                  <option key={item.columnID} value={item.columnID}>{item.col_name}</option>   
+                  ))}
+                </Select>
+            </div>
+
+            <div><h4>Beam Section:</h4></div>
+            <div><Select style={ {width:'100%'}}>
+                  {beamData.map((item) => (
+                  <option key={item.beamID} value={item.beamID}>{item.beam_name}</option>   
+                  ))}
                 </Select>
             </div>
           </div>
-      </div>
+          {/* Section End */}
+          {/* Section Start  */}
+          <h3>Factored Loads</h3>
+          <div className='component-grid    '> 
+              <div><h4>Shear Force(kN) :</h4></div>
+              <div><Input type="text" name="fileName"/></div>
 
+              <div><h4>Axial Force(kN) :</h4></div>
+              <div><Input type="text" name="fileName"/></div>
+          </div>
+          {/* Section End */}
+          {/* Section Start */}
+          <h3>Bolt</h3>
+          <div className='component-grid    '> 
+          <div><h4>Beam Section:</h4></div>
+            <div><Select style={ {width:'100%'}}>
+                  <option value="Customized">Customized</option>
+                  <option value="All">All</option>   
+                 </Select>
+            </div>
+
+            <div><h4>Type:</h4></div>
+            <div><Select style={ {width:'100%'}}>
+                  <option value="Bearing_Bolt">Bearing Bolt</option>
+                  <option value="Fraction_Grip_Bolt">Fraction Grip Bolt</option>   
+                 </Select>
+            </div>
+
+            <div><h4>Property Class:</h4></div>
+            <div><Select style={ {width:'100%'}}>
+                  <option value="Customized">Customized</option>
+                  <option value="All">All</option>   
+                 </Select>
+            </div>
+          </div>
+          {/* Section End */}
+          <h3>Plate</h3>
+          <div className='component-grid    '> 
+          <div><h4>Thickness(mm)</h4></div>
+            <div><Select style={ {width:'100%'}}>
+                  <option value="Customized">Customized</option>
+                  <option value="All">All</option>   
+                 </Select>
+            </div>
+            </div>
+          
+      </div>
+          <div className='inputdock-btn'>
+            <Input type="button" value="Reset" />
+            <Input type="button" value="Design" />
+          </div>
+      </div>              
       {/* Middle */}
       <div className='superMainBody_mid'>
-      <img src={img1} alt="Demo" height='300px' width='300px' /> 
-      <br/>
-      <div>
-
-      <ul>
-{/* Code 1++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
-
-      {/* {data.map((item) => (
-        <p key={item.DesignId}>Save name :{item.DesignName}
+        <img src={img1} alt="Demo" height='300px' width='300px' /> 
         <br/>
-        File Name: {item.PhotoFileName}
-        </p>   
-      ))} */}
-
-{/* Solution ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */}
-      
-<select name="Cars" size="5" >  
-      {logData.map((item) => (
-         <option key={item.logID} value={item.logID}> Save Name : {item.logID}{" : "}
-          File Name: {item.log}  </option>   
-      ))}
-  
-</select>
-
-      </ul>
-
-      </div>
-      
-      
+        <div>
+        <ul>
+            <select name="Cars" size="5" >  
+                  {logData.map((item) => (
+                    <option key={item.logID} value={item.logID}> LOG ID : {item.logID}{" :  _"}
+                      LOG_Name: {item.log}  </option>   
+                  ))}
+            </select>
+        </ul>
+        </div>
       </div>
 
       {/* Right */}
-      <div className='component-grid    '> 
-  
-      <div><label>Property Classes :</label></div>
-      <div><Input type="text" name="fileName" disabled/></div>
-  
-
-    {/* <Input type="button" value="Submit" onClick={savedata}/> */}
-    
-    {/* <input type="button" value="Submit" onClick={refreshList}/> */}
+      <div>
+        <h5>Output Dock</h5>
+      <div className='subMainBody scroll-data'> 
+      {/* Section 1 Start */}
+          <h3>Bolt</h3>       
+          <div className='component-grid'> 
+              <div><h4>Diameter (mm)</h4></div>
+              <div><Input type="text" name="bolt_Diameter"/></div>
+              <div><h4>Property Class</h4></div>
+              <div><Input type="text" name="bolt_Property_Class"/></div>
+              <div><h4>Shear Capacity (kN)</h4></div>
+              <div><Input type="text" name="bolt_Shear Capacity"/></div>
+              <div><h4>Capacity (kN)</h4></div>
+              <div><Input type="text" name="bolt_Capacity"/></div>
+              <div><h4>Bolt Force (kN)</h4></div>
+              <div><Input type="text" name="bolt_Bolt_Force"/></div>
+              <div><h4>Bolt Columns (nos)</h4></div>
+              <div><Input type="text" name="boly_Bolt_Columns"/></div>
+              <div><h4>Bolt Rows (nos)</h4></div>
+              <div><Input type="text" name="bolt_Bolt_Rows"/></div>
+              <div><h4>Spacing</h4></div>
+              <div><Input type="button" value="bolt_Spacing"/></div>
+          </div>
+          {/* Section End */}
+          {/* Section 2 Start */}
+          <h3>Plate</h3>       
+          <div className='component-grid    '> 
+              <div><h4>Thickness (mm)</h4></div>
+              <div><Input type="text" name="plate_Thickness"/></div>
+              <div><h4>Hight (mm)</h4></div>
+              <div><Input type="text" name="plate_Hight"/></div>
+              <div><h4>Length (mm)</h4></div>
+              <div><Input type="text" name="plate_Length"/></div>
+              <div><h4>Capacity</h4></div>
+              <div><Input type="button" name="plate_Capacity" value="Spacing Details"/></div>
+          </div>
+          {/* Section End */}
+          {/* Section 3 Start */}
+          <h3>Section Details</h3>       
+          <div className='component-grid    '> 
+              <div><h4>Capacity</h4></div>
+              <div><Input type="button" value="Spacing Details"/></div>
+          </div>
+          {/* Section End */}
+          {/* Section 4 Start */}
+          <h3>Weld</h3>       
+          <div className='component-grid    '> 
+              <div><h4>Size (mm)</h4></div>
+              <div><Input type="text" name="fileName"/></div>
+              <div><h4>Strength (N/mm2)</h4></div>
+              <div><Input type="text" name="fileName"/></div>
+              <div><h4>Stress (N/mm)</h4></div>
+              <div><Input type="text" name="fileName"/></div>
+          </div>
+          {/* Section End */}
+      </div>
+          <div className='outputdock-btn'>
+            <Input type="button" value="Create Design Report" />
+            <Input type="button" value="Save Output" />
+          </div>
       </div>
     </div>
 
