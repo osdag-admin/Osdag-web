3ee89bef6367637464cb344ce35b10a57df57fe6
Atharva Pingale <atharva0300@gmail.com>
2023-06-08 13:01:07 +0530

    added postgres to django, models created, a clean postgres_Intg_osdag sql script created

--- osdag/admin.py
+++ osdag/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+# import models 
+from osdag.models import Anchor_Bolt , Angle_Pitch , Angles , Beams , Bolt , Bolt_fy_fu , CHS , Channels , Columns , EqualAngle , Material, RHS , SHS , UnequalAngle
+
 # Register your models here.
+admin.site.register(Anchor_Bolt)
+admin.site.register(Angle_Pitch)
+admin.site.register(Angles)
+admin.site.register(Beams)
+admin.site.register(Bolt)
+admin.site.register(Bolt_fy_fu)
+admin.site.register(CHS)
+admin.site.register(Channels)
+admin.site.register(Columns)
+admin.site.register(EqualAngle)
+admin.site.register(Material)
+admin.site.register(RHS)
+admin.site.register(SHS)
+admin.site.register(UnequalAngle)
--- osdag_web/settings.py
+++ osdag_web/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'osdag_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres_Intg_osdag', 
+        'USER': 'osdagdeveloper',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
