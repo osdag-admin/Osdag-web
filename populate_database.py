8ed06142a4c1f9b5be87fe8711b436d51044dc22
sai charan <saicharan0662@gmail.com>
2023-06-08 15:34:07 +0530

    state management and db setup

--- populate_database.py
+++ populate_database.py
@@ -1,10 +1,11 @@
-import psycopg2 
+import psycopg2
 from psycopg2 import sql
 
 
-conn = psycopg2.connect(database = 'postgres_Intg_osdag' , host = '127.0.0.1' , user = 'osdagdeveloper' , password = 'password' , port = '5432')
+conn = psycopg2.connect(database='postgres_Intg_osdag', host='127.0.0.1',
+                        user='osdagdeveloper', password='password', port='5432')
 cursor = conn.cursor()
-file = open("ResourceFiles/Database/postgres_Intg_osdag.sql" , "r+")
+file = open("ResourceFiles/Database/postgres_Intg_osdag.sql", "r+")
 print('file contents : ')
 
 data = file.read()
