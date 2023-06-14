a60cf01982806596884d5b4bae6f65b6ceb7c905
sai charan <saicharan0662@gmail.com>
2023-06-14 16:01:35 +0530

    logs end of design msg placement

--- osdagclient/src/components/Logs.jsx
+++ osdagclient/src/components/Logs.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 
+let endOfDesignLog = ""
 const Logs = ({ logs }) => {
 
     return (
         <div className='log-box'>
             {logs && logs.map((log, index) => {
+                if (log.msg.includes('=== End Of Design ===')) {
+                    endOfDesignLog = log.msg
+                    return <></>
+                }
                 return (
-                    <div key={index}>
-                        <p style={{
-                            color: log.type === 'error' ? 'red' : (
-                                log.type === 'warning' ? 'orange' : 'blue'
-                            )
-                        }}>
-                            <span className='log-info-text'>{log.type}: </span>
-                            <span className='log-text'>{log.msg}</span>
-                        </p>
-                    </div>
+                    <p key={index} style={{
+                        color: log.type === 'error' ? 'red' : (
+                            log.type === 'warning' ? 'orange' : 'blue'
+                        )
+                    }}>
+                        <span className='log-info-text'>{log.type}: </span>
+                        <span className='log-text'>{log.msg}</span>
+                    </p>
                 )
             })}
+            {endOfDesignLog && <p style={{ color: 'green' }}>
+                <span className='log-info-text'>INFO: </span>
+                <span className='log-text'>{endOfDesignLog}</span>
+            </p>}
         </div>
     )
 }
