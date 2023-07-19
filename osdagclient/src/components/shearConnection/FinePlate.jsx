6378e3d8d4b4ce9a155c18bfc0db1ac27717d469
Suraj Ranajit Bhosale <surajedu003@gmail.com>
2023-07-19 10:05:24 +0000

    dropdown for nav bar and handle event for all nav content

--- osdagclient/src/components/shearConnection/FinePlate.jsx
+++ osdagclient/src/components/shearConnection/FinePlate.jsx
@@ -20,6 +20,7 @@ import '@react-pdf-viewer/core/lib/styles/index.css';
 
 // import assets 
 import cad_background from '../../assets/cad_empty_image.png'
+import { Tube } from '@react-three/drei';
 
 
 
@@ -31,24 +32,215 @@ const conn_map = {
   "Beam-Beam": "Beam-Beam"
 }
 
+
+
 const MenuItems = [
   {
     label: "File",
+    dropdown: [
+      { name: "Load", shortcut: "Ctrl+L" },
+      { name: "Save Input", shortcut: "Ctrl+S" },
+      { name: "Save Log Messages", shortcut: "Alt+M" },
+      { name: "Create Design Report", shortcut: "Alt+C" },
+      { name: "Save 3D Model", shortcut: "Alt+3" },
+      { name: "Save Cad Image", shortcut: "Alt+1" },
+      { name: "Save Front View", shortcut: "Alt+Shift+F" },
+      { name: "Save Top View", shortcut: "Alt+Shift+T" },
+      { name: "Save Side View", shortcut: "Alt+Shift+S" },
+      { name: "Quit", shortcut: "Shift+Q" }
+    ]
   },
   {
     label: "Edit",
+    dropdown: [
+      { name: "Design Preferences", shortcut: "Alt+P" }
+    ]
   },
   {
     label: "Graphics",
+    dropdown: [
+      { name: "Zoom In", shortcut: "Ctrl+I" },
+      { name: "Zoom Out", shortcut: "Ctrl+O" },
+      { name: "Pan", shortcut: "Ctrl+P" },
+      { name: "Rotate 3D Model", shortcut: "Ctrl+R" },
+      { name: "Model" },
+      { name: "Beam" },
+      { name: "Column" },
+      { name: "FinePlate" },
+      { name: "Change Background" }
+    ]
   },
   {
     label: "Database",
+    dropdown: [
+      { name: "Downloads", options: ["Column", "Beam", "Angle", "Channel"] },
+      { name: "Reset" }
+    ]
   },
   {
     label: "Help",
-  },
-]
+    dropdown: [
+      { name: "Video Tutorials" },
+      { name: "Design Examples" },
+      { name: "Ask us a question" },
+      { name: "About Osdag" }
+    ]
+  }
+];
 
+// Dropdown Navbar
+const DropdownMenu = ({ label, dropdown }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClick = (option) => {
+    // Handle specific code for the clicked option here
+    // console.log(`Clicked option: ${option.name}`);
+    switch (option.name) {
+      case `Load`: console.log(`Load val: ${option.name}`);
+        break;
+
+      case `Save Input`: console.log(`Save val ${option.name}`);
+        break;
+
+      case `Save Log Messages`: console.log(`Save log val ${option.name}`);
+        break;
+
+      case `Create Design Report`: console.log(`Create report val ${option.name}`);
+        break;
+        
+      case `Save 3D Model`: console.log(`Save 3D model val ${option.name}`);
+        break;
+
+      case `Save Cad Image`: console.log(`Save Cad image val ${option.name}`);
+        break;
+
+      case `Save Front View`: console.log(`Save Front View val ${option.name}`);
+        break;
+
+      case `Save Top View`: console.log(`Save Top View val ${option.name}`);
+        break;
+
+      case `Save Side View`: console.log(`Save Side View val ${option.name}`);
+        break;
+
+      case `Quit`: console.log(`Quit val ${option.name}`);
+        break;
+      // File End
+      // Edit Start
+      case `Design Preferences`: 
+      console.log(`Design Preferences ${option.name}`);
+      setDesignPreferencesModel(true);
+        break;
+      // Edit End
+      // Graphics Start
+      case `Zoom In`: console.log(`Zoom In val ${option.name}`);
+        break;
+
+      case `Zoom Out`: console.log(`Zoom Out val ${option.name}`);
+        break;
+
+      case `Pan`: console.log(`Pan val ${option.name}`);
+        break;
+
+      case `Rotate 3D Model`: console.log(`Rotate 3D Model val ${option.name}`);
+        break;
+
+      case `Model`: console.log(`Model val ${option.name}`);
+        break;
+
+      case `Beam`: console.log(`Beam val ${option.name}`);
+        break;
+
+      case `Column`: console.log(`Column val ${option.name}`);
+        break;
+
+      case `FinePlate`: console.log(`FinePlate val ${option.name}`);
+      // Graphics End
+     
+      case `Downloads`: console.log(`Downloads val ${option.name}`);
+        break;
+
+      case `Reset`: console.log(`Reset val ${option.name}`);
+        break;
+      // Database End
+      // Help Start
+      case `Video Tutorials`: console.log(`Video Tutorials val ${option.name}`);
+        break;
+
+      case `Design Examples`: console.log(`Design Examples val ${option.name}`);
+        break;
+
+      case `Ask us a question`: console.log(`Ask us a question val ${option.name}`);
+        break;
+
+      case `About Osdag`: console.log(`About Osdag val ${option.name}`);
+        break;
+      // Help End
+  
+      default: console.log(`Default Val: ${option.name}`);
+        break;
+    }
+
+  };
+
+
+  return (
+    <div className="dropdown">
+      <div className="dropdown-label" onClick={handleToggle}>
+        {label}
+      </div>
+      {isOpen && (
+        <div className="dropdown-menu">
+          {dropdown.map((option, index) => (
+            <div className="dropdown-items" key={index} onClick={() => handleClick(option)}>
+              {option.name}
+              {option.shortcut && <span className="shortcut">{option.shortcut}</span>}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+// pop up handler 
+// Design Pref
+
+
+
+// End 
+// Key event 
+// const KeyPressListener = () => {
+//   useEffect(() => {
+//     const handleKeyDown = (event) => {
+//       // Check for key combinations
+//       if (event.altKey && event.key === 'p') {
+        
+//         console.log('Alt + P pressed');
+//       } 
+
+//       // Listen for individual key presses
+//       switch (event.key) {
+//         case 'Enter':
+//           console.log('Enter key pressed');
+//           break;
+//         case 'Escape':
+//           console.log('Escape key pressed');
+//           break;
+//         default:
+//           break;
+//       }
+//     };
+
+//     window.addEventListener('keydown', handleKeyDown);
+
+//   }, []);
+// };
+// end
 
 function FinePlate() {
 
@@ -597,16 +789,24 @@ function FinePlate() {
     }
   }, [])
 
+  const [isDesignPreferencesModelOpen, setDesignPreferencesModel] = useState(false);
+
+  const closeDesignPreferencesModel = () => {
+    setDesignPreferencesModel(false);
+  };
+  
+
+
   return (
     <>
       <div style={{width: '100%'}}>
-        <div className='module_nav'>
-
-          {MenuItems.map((item, index) => (
-            <div key={index}>{item.label}</div>
-          ))}
-        </div>
-
+      <div className="module_nav">
+      {MenuItems.map((item, index) => (
+        <DropdownMenu key={index} label={item.label} dropdown={item.dropdown} />
+      ))}
+    </div>
+    {/* <KeyPressListener />
+     */}
         {/* Main Body of code  */}
         <div className='superMainBody'>
           {/* Left */}
@@ -1000,6 +1200,22 @@ function FinePlate() {
                 </div>
               </Modal>
 
+              {/* Nav Bar Model list */}
+              {isDesignPreferencesModelOpen && (
+              <Modal
+                visible={isDesignPreferencesModelOpen}
+                onCancel={() => setDesignPreferencesModel(false)}
+                footer={null}
+                width={700}
+                height={700}
+              >
+                <div>
+                  <h1>Design pref</h1>
+                </div>
+              </Modal>
+    )}
+
+                  {/* Nav Bar Model List End */}
 
             </div>
           </div>
