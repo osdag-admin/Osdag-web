99548aca0681819aee0a9709c11b8f30a87b7503
Suraj Ranajit Bhosale <surajedu003@gmail.com>
2023-08-04 07:08:40 +0000

    add user dashboard and UI

--- osdagclient/src/components/userAccount/UserAccount.jsx
+++ osdagclient/src/components/userAccount/UserAccount.jsx
@@ -1,19 +1,60 @@
 import React from 'react';
-import './UserAccount.css'; // Import your CSS file
+import { List, Button } from 'antd';
+import './UserAccount.css'; 
 
-function UserAccount() {
+const UserAccount = () => {
+  // Replace these with your actual data
+  const userEmail = 'user@example.com';
+  const osiFiles = [
+    { name: 'File 1.osi', url: 'https://example.com/file1.osi' },
+    { name: 'File 2.osi', url: 'https://example.com/file2.osi' },
+    // Add more fila
+  ];
+
+  const onViewClick = (url) => {
+    // Implement view logic here
+    console.log('View file:', url);
+  };
+
+  const onDownloadClick = (url) => {
+    // Implement download logic here
+    console.log('Download file:', url);
+  };
+
+  const renderItem = (file) => (
+    <List.Item>
+      <span>{file.name}</span>
+      <Button onClick={() => onViewClick(file.url)} style={{ marginLeft: '10px' }}>
+        View
+      </Button>
+      <Button onClick={() => onDownloadClick(file.url)} style={{ marginLeft: '10px' }}>
+        Download
+      </Button>
+    </List.Item>
+  );
+
   return (
-    <div className="dashboard-container">
-      <div className="main-content">
+    <>
+    <div className="user-dashboard-container">
+    <div className='dashboard-header'>
+        <h2>User Dashboard</h2>
         <div>
-            <img src="" alt="temp" />
+          <strong>Email:</strong> {userEmail}
         </div>
+    </div>
+      <div className="user-dashboard">
         <div>
-            <label>Company Name :</label>
+          <strong>Files in .osi format:</strong>
+          <List
+            dataSource={osiFiles}
+            renderItem={renderItem}
+            className="file-list" 
+            />
         </div>
       </div>
     </div>
+            </>
   );
-}
+};
 
 export default UserAccount;
