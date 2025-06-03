f9a5d4a57c2fa819d6fc84f88aefb21182b5d8c7
syedfaran4 <154878224+faran4@users.noreply.github.com>
2025-06-03 15:56:15 +0530

    Update EngineeringModule.jsx

--- osdagclient/src/modules/shared/components/EngineeringModule.jsx
+++ osdagclient/src/modules/shared/components/EngineeringModule.jsx
@@ -238,6 +238,7 @@ export const EngineeringModule = ({
         </div>
       </div>
 
+      {/* Designreport separate modal */}
       <DesignReportModal
         isOpen={createDesignReportBool}
         onCancel={handleCancelDesignReport}
