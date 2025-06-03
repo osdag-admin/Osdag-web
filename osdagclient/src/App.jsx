44e0176e67a3d92ec7e329b96d75e26ac0da1dee
faran4 <faranimam4@gmail.com>
2025-06-03 11:18:18 +0530

    Restructuring Code

--- osdagclient/src/App.jsx
+++ osdagclient/src/App.jsx
@@ -27,10 +27,9 @@ import jwt_decode from 'jwt-decode';
 import EndPlate from './components/shearConnection/EndPlate';
 import CleatAngle from './components/shearConnection/CleatAngle';
 import SeatedAngle from './components/shearConnection/SeatedAngle';
-import CoverPlateBolted from "./components/momentConnection/beamToBeamSplice/CoverPlateBolted";
-import BeamBeamEndPlate from "./components/momentConnection/beamToBeamSplice/BeamBeamEndPlate";
+import CoverPlateBolted from "./modules/coverPlateBolted/CoverPlateBolted";
+import BeamBeamEndPlate from "./modules/beamBeamEndPlate/BeamBeamEndPlate";
 import BoltedToEndPage from './components/TensionMembers/BoltedToEnd/pages/BoltedToEndPage';
-
 import CoverPlateWelded from "./components/momentConnection/beamToBeamSplice/CoverPlateWelded";
 import BeamToColumnEndPlate from "./components/momentConnection/BeamToColumnEndPlate";
 
